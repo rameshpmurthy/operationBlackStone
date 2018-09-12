7023afb60c47cbf9257efcf43f9c9e82c8f02e51
ramesh.p <ramesh.p@LP-090-Ramesh.UTG.local>
2018-09-12 16:41:02 +0530

    First Commit. Master

---
+++ BlackStone1/src/com/learning/blackstone/in/Main.java
@@ -0,0 +1,14 @@
+package com.learning.blackstone.in;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		for (int count=1; count <=5 ; count++)
+		{
+			System.out.println( count + "  Mississippi");
+		}
+
+	}
+
+}
